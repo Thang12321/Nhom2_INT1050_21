048e0825db5a8f49ea0f5efa7c98f841e6ebe82a
tavanthang <71245250+Thang12321@users.noreply.github.com>
2022-03-12 14:02:30 +0700

    Update primeCount.cpp

--- Tuan4/primeCount.cpp
+++ Tuan4/primeCount.cpp
@@ -13,15 +13,23 @@ bool checkPrime(int n)
 }
 int main()
 {
-    int n, count = 0;
+    int n, count1 = 0, count2 = 0;
     // n = 443
     cin >> n;
     for (int i = 2; i < n; i++)
     {
         if (checkPrime(i) == true)
         {
-            count++;
+            count1++;
         }
     }
-    cout << count;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (checkPrime(i) == true)
+        {
+            count2++;
+        }
+    }
+    cout << "Phuong phap 2: " << count1 << endl;
+    cout << "Phuong phap 3: " << count2 << endl;
 }
